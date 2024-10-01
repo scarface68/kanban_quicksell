93412c8ce58f89f518fd131deff4043afec818da
Sai Chandan Mohanty <saichandan68@gmail.com>
2024-10-01 18:13:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react"
+import Header from "./components/Header"
+import Grid from "./components/Grid"
+import { GET_TICKETS_URL } from "./constants"
+import { loadGrid, mapUsersByUserId } from "./utils"
+import Loader from "./components/Loader"
+import "./App.css"
 
 function App() {
+  const [tickets, setTickets] = useState([])
+  const [userData, setUserData] = useState({})
+  const [gridData, setGridData] = useState({})
+  const [grouping, setGrouping] = useState("status")
+  const [ordering, setOrdering] = useState("priority")
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    loadSettings()
+    fetch(GET_TICKETS_URL)
+      .then(resp => resp.json())
+      .then(res => {
+        const { tickets, users } = res
+        setTickets(tickets)
+        setUserData(mapUsersByUserId(users))
+      })
+      .catch(err => {})
+  }, [])
+
+  useEffect(() => {
+    if (!tickets.length) return
+    setGridData(loadGrid(tickets, grouping, ordering))
+    setLoading(false)
+  }, [grouping, ordering, tickets])
+
+  const onSetGrouping = useCallback(value => {
+    setLoading(true)
+    setGrouping(value)
+    saveSettings({ grouping: value })
+  }, [])
+
+  const onSetOrdering = useCallback(value => {
+    setLoading(true)
+    setOrdering(value)
+    saveSettings({ ordering: value })
+  }, [])
+
+  const saveSettings = useCallback(data => {
+    for (let key in data) localStorage.setItem(key, data[key])
+  }, [])
+
+  const loadSettings = useCallback(() => {
+    setGrouping(localStorage.getItem("grouping") || "status")
+    setOrdering(localStorage.getItem("ordering") || "priority")
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+        grouping={grouping}
+        setGrouping={onSetGrouping}
+        ordering={ordering}
+        setOrdering={onSetOrdering}
+      />
+      {loading ? (
+        <Loader />
+      ) : (
+        <Grid gridData={gridData} grouping={grouping} userIdToData={userData} />
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
